0bfd2a24f12ae892f393d5e94af6e55e857ca24b
Matt Hippely <mhippely@vmware.com>
2020-06-26 12:20:15 -0700

    build: testing directory path
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -5,6 +5,9 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
 # update website for production storybook
-sed -i '' -e 's/\/localhost:6006/storybook\/angular/g' ./dist/website/assets/js/*.js
+ls -al dist/website
+ls -al dist/website/assets
+ls -al dist/website/assets/js
+sed -i '' -e 's/\/localhost:6006/storybook\/angular/g' dist/website/assets/js/*.js
 
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
