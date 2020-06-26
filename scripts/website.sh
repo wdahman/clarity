28a0d20c85d4fefd8f952c2ef1ad9931c6420f92
Matt Hippely <mhippely@vmware.com>
2020-06-26 15:17:44 -0700

    build: remove q from deploy script
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -5,8 +5,9 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
 # update website for production storybook urls
+# NOTE: this leave core untouched b/c vuepress code uses the //localhost:6006 url and we are
+// replacing the '/localhost:6006' part with 'storybook/angular'
 find . -type f -name "*.html" -print0 | xargs -0 perl -pi -e 's/\/localhost:6006/storybook\/angular/g'
 find . -type f -name "*.js" -print0 | xargs -0 perl -pi -e 's/\/localhost:6006/storybook\/angular/g'
 
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
-q
