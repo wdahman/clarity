74a8acb58d9febf7982224991e6018cdece34f9d
Matt Hippely <mhippely@vmware.com>
2020-06-26 13:26:45 -0700

    build: trying OS detection for deploy script
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -10,6 +10,9 @@ pwd
 ls -al dist/website
 ls -al dist/website/assets
 ls -al dist/website/assets/js
-sed -i '' -e 's/\/localhost:6006/storybook\/angular/g' dist/website/assets/js/*.js
+if [[$OSTYPE == "darwin"*]]; then
+  sed -i '' -e 's/\/localhost:6006/storybook\/angular/g' dist/website/assets/js/*.js
+else
+  sed -i -e 's/\/localhost:6006/storybook\/angular/g' dist/website/assets/js/*.js
 
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
