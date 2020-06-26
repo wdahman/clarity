30c95cbf6690a8e78a0278f750672f3bf2d92c83
Matt Hippely <mhippely@vmware.com>
2020-06-26 12:03:25 -0700

    build: try 2 at publish for the angular storybook demos
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -3,4 +3,8 @@
 # This downloads a set of key-value env variables, but blocks any requests that aren't from GitHub Actions
 curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
 # dotenv loads up the env variables into the shell, then deploys through Netlify
+
+# update website for production storybook
+sed -i '' -e 's/\/localhost:6006/storybook\/angular/g' ./dist/website/assets/js/*.js
+
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
