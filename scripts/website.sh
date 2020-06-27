c86b6960c011a1157b77d8fb012c67eae5670d58
Matt Hippely <mhippely@vmware.com>
2020-06-26 17:08:31 -0700

    build: update replacement string for prod build
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -8,6 +8,6 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # NOTE: this leave core untouched b/c vuepress code uses the //localhost:6006 url and we are
 #       replacing the '/localhost:6006' part with 'storybook/angular'
 find . -type f -name "*.html" -print0 | xargs -0 perl -pi -e 's/\/localhost:6006/storybook\/angular/g'
-find . -type f -name "*.js" -print0 | xargs -0 perl -pi -e 's/\/localhost:6006/storybook\/angular/g'
+find . -type f -name "*.js" -print0 | xargs -0 perl -pi -e 's/\/localhost:6006/storybook\/angular\/iframe.html/g'
 
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
