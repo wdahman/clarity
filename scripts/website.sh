813dc76fc43f7a266a69ef473fdb22765d762abf
Matt Hippely <mhippely@vmware.com>
2020-06-29 09:38:48 -0700

    build: fixes local deploy, testing deply w/ CI
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -5,9 +5,9 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
 # update website for production storybook urls
-# NOTE: this leave core untouched b/c vuepress code uses the //localhost:6006 url and we are
+# NOTE: this leaves core untouched b/c vuepress code uses the //localhost:6006 url and we are
 # replacing the '/localhost:6006' part with 'storybook/angular'
 find . -type f -name "*.html" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
-find . -type f -name "*.js" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:600\//storybook\/angular\/index.html/g'
+find . -type f -name "*.js" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
 
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
