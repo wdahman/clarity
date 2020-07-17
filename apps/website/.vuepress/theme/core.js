bd13c63c314e0d96f0d9f575145ad807552a0972
Matt Hippely <mhippely@vmware.com>
2020-07-17 11:56:38 -0700

    build: test without core icon shapes
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- apps/website/.vuepress/theme/core.js
+++ apps/website/.vuepress/theme/core.js
@@ -2,27 +2,27 @@ import '@clr/core/alert/register.js';
 import '@clr/core/button/register.js';
 import '@clr/core/icon/register.js';
 
-import {
-  loadCoreIconSet,
-  loadChartIconSet,
-  loadCommerceIconSet,
-  loadEssentialIconSet,
-  loadMediaIconSet,
-  loadSocialIconSet,
-  loadTechnologyIconSet,
-  loadTextEditIconSet,
-  loadTravelIconSet,
-} from '@clr/core/icon-shapes';
-
-loadCoreIconSet();
-loadChartIconSet();
-loadEssentialIconSet();
-loadCommerceIconSet();
-loadMediaIconSet();
-loadSocialIconSet();
-loadTechnologyIconSet();
-loadTextEditIconSet();
-loadTravelIconSet();
+// import {
+//   loadCoreIconSet,
+//   loadChartIconSet,
+//   loadCommerceIconSet,
+//   loadEssentialIconSet,
+//   loadMediaIconSet,
+//   loadSocialIconSet,
+//   loadTechnologyIconSet,
+//   loadTextEditIconSet,
+//   loadTravelIconSet,
+// } from '@clr/core/icon-shapes';
+//
+// loadCoreIconSet();
+// loadChartIconSet();
+// loadEssentialIconSet();
+// loadCommerceIconSet();
+// loadMediaIconSet();
+// loadSocialIconSet();
+// loadTechnologyIconSet();
+// loadTextEditIconSet();
+// loadTravelIconSet();
 
 import '@clr/icons';
 import '@clr/icons/shapes/all-shapes';
