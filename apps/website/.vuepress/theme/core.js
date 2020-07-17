0a3488cf39485c2b5e54536e21c6bbb8c0dd4422
Matt Hippely <mhippely@vmware.com>
2020-07-16 17:23:57 -0700

    build(website): remove core imports for now
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- apps/website/.vuepress/theme/core.js
+++ apps/website/.vuepress/theme/core.js
@@ -1,28 +1,28 @@
-import '@clr/core/alert';
-import '@clr/core/button';
-import '@clr/core/icon';
-
-import {
-  loadCoreIconSet,
-  loadChartIconSet,
-  loadCommerceIconSet,
-  loadEssentialIconSet,
-  loadMediaIconSet,
-  loadSocialIconSet,
-  loadTechnologyIconSet,
-  loadTextEditIconSet,
-  loadTravelIconSet,
-} from '@clr/core/icon-shapes';
-
-loadCoreIconSet();
-loadChartIconSet();
-loadEssentialIconSet();
-loadCommerceIconSet();
-loadMediaIconSet();
-loadSocialIconSet();
-loadTechnologyIconSet();
-loadTextEditIconSet();
-loadTravelIconSet();
+// import '@clr/core/alert';
+// import '@clr/core/button';
+// import '@clr/core/icon';
+//
+// import {
+//   loadCoreIconSet,
+//   loadChartIconSet,
+//   loadCommerceIconSet,
+//   loadEssentialIconSet,
+//   loadMediaIconSet,
+//   loadSocialIconSet,
+//   loadTechnologyIconSet,
+//   loadTextEditIconSet,
+//   loadTravelIconSet,
+// } from '@clr/core/icon-shapes';
+//
+// loadCoreIconSet();
+// loadChartIconSet();
+// loadEssentialIconSet();
+// loadCommerceIconSet();
+// loadMediaIconSet();
+// loadSocialIconSet();
+// loadTechnologyIconSet();
+// loadTextEditIconSet();
+// loadTravelIconSet();
 
 import '@clr/icons';
 import '@clr/icons/shapes/all-shapes';
