45c32cbcad9dcce37b442432b108fc8b561b2f60
Stanimira Vlaeva <svlaeva@vmware.com>
2020-11-02 14:49:55 +0200

    feat: add badges, labels and lists snippets to the migration app
    Signed-off-by: Stanimira Vlaeva <svlaeva@vmware.com>

--- apps/clarity-migration/src/app/app-routing.module.ts
+++ apps/clarity-migration/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { AlertPage } from './pages/alert/alert.page';
 import { ButtonPage } from './pages/button/button.page';
 import { HomePage } from './pages/home/home.page';
+import { BadgePage } from './pages/badge/badge.page';
+import { LabelPage } from './pages/label/label.page';
+import { ListPage } from './pages/list/list.page';
 
 const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'alert', component: AlertPage },
+  { path: 'badge', component: BadgePage },
   { path: 'button', component: ButtonPage },
+  { path: 'label', component: LabelPage },
+  { path: 'list', component: ListPage },
 ];
 
 @NgModule({
--- apps/clarity-migration/src/app/app.module.ts
+++ apps/clarity-migration/src/app/app.module.ts
@@ -7,8 +7,11 @@ import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomePage } from './pages/home/home.page';
-import { ButtonPage } from './pages/button/button.page';
 import { AlertPage } from './pages/alert/alert.page';
+import { ButtonPage } from './pages/button/button.page';
+import { BadgePage } from './pages/badge/badge.page';
+import { LabelPage } from './pages/label/label.page';
+import { ListPage } from './pages/list/list.page';
 
 import { SourceCodeComponent } from './components/sourcecode.component';
 import { DynamicHostDirective } from './components/dynamic/dynamic-host-directive';
@@ -20,9 +23,12 @@ import { ExampleWrapper } from './components/example-wrapper';
     AppComponent,
 
     /* pages */
-    AlertPage,
     HomePage,
+    AlertPage,
+    BadgePage,
     ButtonPage,
+    LabelPage,
+    ListPage,
 
     /* helpers */
     SourceCodeComponent,
